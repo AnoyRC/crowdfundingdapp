9d5987146049a1d56072c8d1a433fd90b16c291d
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2022-12-17 00:50:04 +0530

    Withdraw function completion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,62 +1,78 @@
-import './App.css';
-import {useState, useEffect} from "react";
-import idl from "./idl.json"
-import{ Connection, PublicKey, clusterApiUrl} from "@solana/web3.js"
-import{ Program, AnchorProvider, web3, utils, BN} from "@project-serum/anchor"
-import { Buffer } from "buffer"
-import { publicKey } from '@project-serum/anchor/dist/cjs/utils';
+import "./App.css";
+import idl from "./idl.json";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import {
+	Program,
+	AnchorProvider,
+	web3,
+	utils,
+	BN,
+} from "@project-serum/anchor";
+import { useEffect, useState } from "react";
+import { Buffer } from "buffer";
 window.Buffer = Buffer;
 
 const programID = new PublicKey(idl.metadata.address);
 const network = clusterApiUrl("devnet");
 const opts = {
-  preflightCommitment: "processed"
-}
+	preflightCommitment: "processed",
+};
 const { SystemProgram } = web3;
 
 const App = () => {
-  const [walletAddress, setWalletAddress] = useState(null);
-  const [campaigns,setCampaigns] = useState([])
+	const [walletAddress, setWalletAddress] = useState(null);
+	const [campaigns, setCampaigns] = useState([]);
 
-  const getProvider = () => {
-    const connection = new Connection(network, opts.preflightCommitment)
-    const provider = new AnchorProvider(connection, window.solana, opts.preflightCommitment)
-    return provider;
-  }
+	const getProvider = () => {
+		const connection = new Connection(network, opts.preflightCommitment);
+		const provider = new AnchorProvider(
+			connection,
+			window.solana,
+			opts.preflightCommitment
+		);
+		return provider;
+	};
+  
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { solana } = window;
+			if (solana) {
+				if (solana.isPhantom) {
+					console.log("Phantom wallet found!");
+					const response = await solana.connect({
+						onlyIfTrusted: true,
+					});
+					console.log(
+						"Connected with public key:",
+						response.publicKey.toString()
+					);
+					setWalletAddress(response.publicKey.toString());
+				}
+			} else {
+				alert("Solana object not found! Get a Phantom wallet");
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  const checkIfWalletIsConnected = async() => {
-    try {
-      const {solana} = window;
-      if(solana) {
-        if(solana.isPhantom){
-          console.log("Phantom wallet found!")
-          const response = await solana.connect({
-            onlyIfTrusted : true
-          })
-          console.log("Connected to public key : ", response.publicKey.toString())
-          setWalletAddress(response.publicKey.toString())
-        }
-      }else{
-        alert("Solana object not found! Get a phantom wallet");
-      }
-    } catch (err) {
-      console.log(err)
-    }
-  }
+	const connectWallet = async () => {
+		const { solana } = window;
+		if (solana) {
+			const response = await solana.connect();
+			console.log(
+				"Connected with public key:",
+				response.publicKey.toString()
+			);
+			setWalletAddress(response.publicKey.toString());
+		}
+	};
 
-  const connectWallet = async () => {
-    const {solana} = window;
-    if(solana){
-      const response = await solana.connect()
-      console.log('Connected with public key:', response.publicKey.toString())
-    }
-  };
-
-  const getCampaigns = async() => {
-    const connection = new Connection(network, opts.preflightCommitment)
-    const provider = getProvider()
-    const program = new Program(idl, programID, provider)
-    Promise.all(
+	const getCampaigns = async () => {
+		const connection = new Connection(network, opts.preflightCommitment);
+		const provider = getProvider();
+		const program = new Program(idl, programID, provider);
+		Promise.all(
 			(await connection.getProgramAccounts(programID)).map(
 				async (campaign) => ({
 					...(await program.account.campaign.fetch(campaign.pubkey)),
@@ -64,64 +80,82 @@ const App = () => {
 				})
 			)
 		).then((campaigns) => setCampaigns(campaigns));
-  }
+	};
 
-  const createCampaign = async() => {
-    try {
-      const provider = getProvider()
-      const program = new Program(idl, programID, provider)
-      const [campaign] = await PublicKey.findProgramAddressSync(
+	const createCampaign = async () => {
+		try {
+			const provider = getProvider();
+			const program = new Program(idl, programID, provider);
+			const [campaign] = await PublicKey.findProgramAddress(
 				[
 					utils.bytes.utf8.encode("CAMPAIGN_DEMO"),
 					provider.wallet.publicKey.toBuffer(),
 				],
 				program.programId
 			);
-      await program.rpc.create('campaign name', 'campaign description',{
-        accounts: {
-          campaign,
-          user : provider.wallet.publicKey,
-          systemProgram: SystemProgram.programId,
-        }
-      })
-      console.log("Created a new campaign w/ address",campaign.toString())
-    } catch (err) {
-      console.log(err)
-    }
-  }
+			await program.rpc.create("campaign name", "campaign description", {
+				accounts: {
+					campaign,
+					user: provider.wallet.publicKey,
+					systemProgram: SystemProgram.programId,
+				},
+			});
+			console.log(
+				"Created a new campaign w/ address:",
+				campaign.toString()
+			);
+		} catch (error) {
+			console.error("Error creating campaign account:", error);
+		}
+	};
 
-  const donate = async (publicKey) => {
-    try {
-      const provider = getProvider()
-      const program = new Program(idl, programID, provider)
+	const donate = async (publicKey) => {
+		try {
+			const provider = getProvider();
+			const program = new Program(idl, programID, provider);
 
-      await program.rpc.donate(new BN(0.2 * web3.LAMPORTS_PER_SOL),{
-        accounts:{
-          campaign: publicKey,
-          user:provider.wallet.publicKey,
-          systemProgram: SystemProgram.programId
-        }
-      })
-      console.log("Donated some money to : ", publicKey.toString())
-      getCampaigns()
-    } catch (error) {
-      console.error("Error donatiing:" , error)
-    }
-  }
+			await program.rpc.donate(new BN(0.2 * web3.LAMPORTS_PER_SOL), {
+				accounts: {
+					campaign: publicKey,
+					user: provider.wallet.publicKey,
+					systemProgram: SystemProgram.programId,
+				},
+			});
+			console.log("Donated some money to:", publicKey.toString());
+			getCampaigns();
+		} catch (error) {
+			console.error("Error donating:", error);
+		}
+	};
 
-  
+	const withdraw = async (publicKey) => {
+		try {
+			const provider = getProvider();
+			const program = new Program(idl, programID, provider);
+			await program.rpc.withdraw(new BN(0.2 * web3.LAMPORTS_PER_SOL), {
+				accounts: {
+					campaign: publicKey,
+					user: provider.wallet.publicKey,
+				},
+			});
+			console.log("Withdrew some money from:", publicKey.toString());
+		} catch (error) {
+			console.error("Error withdrawing:", error);
+		}
+	};
 
-  const renderNotConnectedContainer = () => {
-    return <button onClick={connectWallet}>Connect to Wallet</button>
-  }
+	const renderNotConnectedContainer = () => (
+		<button onClick={connectWallet}>Connect to Wallet</button>
+	);
 
-  const renderConnectedContainer = () => {
-    return <>
-      <button onClick={createCampaign}>Create a campaign</button>
-      <button onClick={getCampaigns}>Get a list of campaigns</button>
-      <br />
-      {campaigns.map(campaign => (<>
-        <p>Campaign ID: {campaign.pubkey.toString()}</p>
+	const renderConnectedContainer = () => (
+		<>
+			<button onClick={createCampaign}>Create a campaign…</button>
+			<button onClick={getCampaigns}>Get a list of campaigns…</button>
+			<br />
+			{campaigns.map((campaign) => (
+				<>
+					<p>Campaign ID: {campaign.pubkey.toString()}</p>
 					<p>
 						Balance:{" "}
 						{(
@@ -130,24 +164,33 @@ const App = () => {
 					</p>
 					<p>{campaign.name}</p>
 					<p>{campaign.description}</p>
-          <button onClick={() => donate(campaign.pubkey)}>Click to donate!</button>
-      </>))}
-    </>
-  }
+					<button onClick={() => donate(campaign.pubkey)}>
+						Click to donate!
+					</button>
+					<button onClick={() => withdraw(campaign.pubkey)}>
+						Click to withdraw!
+					</button>
+					<br />
+				</>
+			))}
+		</>
+	);
   
-  useEffect(() => {
-    const onLoad = async() => {
-      await checkIfWalletIsConnected()
-    }
-    window.addEventListener('load',onLoad)
-    return () => window.removeEventListener('load', onLoad)
-  }, [])
+	useEffect(() => {
+		const onLoad = async () => {
+			await checkIfWalletIsConnected();
+		};
+		window.addEventListener("load", onLoad);
+		return () => window.removeEventListener("load", onLoad);
+	}, []);
 
-  return <div className='App'>
-    {!walletAddress && renderNotConnectedContainer()}
-    {walletAddress && renderConnectedContainer()}
-  </div>
-  
-}
+	return (
+		<div className="App">
+			{!walletAddress && renderNotConnectedContainer()}
+			{walletAddress && renderConnectedContainer()}
+		</div>
+	);
+};
 
 export default App;
+
