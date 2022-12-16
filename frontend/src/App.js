2b39dafac72c5d1344eea3e19c85894c91e09fbd
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2022-12-17 00:16:35 +0530

    List all the campaigns

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ const { SystemProgram } = web3;
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
+  const [campaigns,setCampaigns] = useState([])
 
   const getProvider = () => {
     const connection = new Connection(network, opts.preflightCommitment)
@@ -50,6 +51,20 @@ const App = () => {
     }
   };
 
+  const getCampaigns = async() => {
+    const connection = new Connection(network, opts.preflightCommitment)
+    const provider = getProvider()
+    const program = new Program(idl, programID, provider)
+    Promise.all(
+			(await connection.getProgramAccounts(programID)).map(
+				async (campaign) => ({
+					...(await program.account.campaign.fetch(campaign.pubkey)),
+					pubkey: campaign.pubkey,
+				})
+			)
+		).then((campaigns) => setCampaigns(campaigns));
+  }
+
   const createCampaign = async() => {
     try {
       const provider = getProvider()
@@ -79,7 +94,22 @@ const App = () => {
   }
 
   const renderConnectedContainer = () => {
-    return <button onClick={createCampaign}>Create a campaign</button>
+    return <>
+      <button onClick={createCampaign}>Create a campaign</button>
+      <button onClick={getCampaigns}>Get a list of campaigns</button>
+      <br />
+      {campaigns.map(campaign => (<>
+        <p>Campaign ID: {campaign.pubkey.toString()}</p>
+					<p>
+						Balance:{" "}
+						{(
+							campaign.amountDonated / web3.LAMPORTS_PER_SOL
+						).toString()}
+					</p>
+					<p>{campaign.name}</p>
+					<p>{campaign.description}</p>
+      </>))}
+    </>
   }
   
   useEffect(() => {
